9253861c8650cebc11a1981341fde8a5dc8f7a25
ArturoEsp <55172579+ArturoEsp@users.noreply.github.com>
2023-08-07 17:39:09 -0600

    first commit

---
+++ src/main/java/org/acme/movimientos/dtos/BodyResp.java
@@ -0,0 +1,24 @@
+package org.acme.movimientos.dtos;
+
+public class BodyResp {
+	
+	public BodyResp() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	private AccountTransactionsResp accountTransactionsRes;
+
+	public AccountTransactionsResp getAccountTransactionsRes() {
+		return accountTransactionsRes;
+	}
+
+	public void setAccountTransactionsRes(AccountTransactionsResp accountTransactionsRes) {
+		this.accountTransactionsRes = accountTransactionsRes;
+	}
+
+	
+
+	
+	
+}
