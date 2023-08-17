f3ede7232c7b9efd0d5265b675103604f788a3c0
ArturoEsp <55172579+ArturoEsp@users.noreply.github.com>
2023-08-17 11:45:42 -0600

    Add files via upload

--- src/main/java/org/acme/movimientos/dtos/InfoDto.java
+++ src/main/java/org/acme/movimientos/dtos/InfoDto.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.UUID;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
@@ -24,30 +23,13 @@ public class InfoDto implements Serializable{
     //Objeto de Respuesta
     private Map<?,?> data;
 
-    //Se integro para validar el token que se ingresa desde api manager, se quitara despues
-    private String token;
-    
     public void setData(Object a){
         this.data = convertObjectToMap(a);
     }
     public static Map<String, Object> convertObjectToMap(Object object) {
         ObjectMapper objectMapper = new ObjectMapper();
-		if (object instanceof String) {
-            try {
-                // Si es una cadena, primero deserializamos el JSON en un Map
-                return objectMapper.readValue((String) object, new TypeReference<Map<String, Object>>() {});
-            } catch (Exception e) {
-				System.out.println("Error al deserializar "+e.getMessage());
-                throw new RuntimeException("Error al deserializar la cadena JSON en un Map", e);
-            }
-        }
-        
-        // Si no es una cadena, intentamos convertir directamente el objeto en un Map
         return objectMapper.convertValue(object, Map.class);
     }
-	public Map<?,?> getData() {
-		return data;
-	}
 	public String getTransactionID() {
 		return transactionID;
 	}
@@ -74,12 +56,6 @@ public class InfoDto implements Serializable{
 	}
 	public static long getSerialversionuid() {
 		return serialVersionUID;
-	}
-	public String getToken() {
-		return token;
-	}
-	public void setToken(String token) {
-		this.token = token;
 	}
     
     
