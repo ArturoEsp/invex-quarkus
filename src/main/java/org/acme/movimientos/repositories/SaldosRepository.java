b29d42c111ed8e636c73626ae15edf65aed14a1f
ArturoEsp <ext_aespinoza@invex.com>
2023-08-17 11:58:40 -0600

    chore: elimina files

--- src/main/java/org/acme/movimientos/repositories/SaldosRepository.java
+++
@@ -1,9 +0,0 @@
-package org.acme.movimientos.repositories;
-
-//import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class SaldosRepository{ // implements PanacheRepository<Saldos> {
-
-}
